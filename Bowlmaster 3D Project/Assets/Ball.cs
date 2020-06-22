8aa8770455979f8726759471258c1186a90b1c1f
david <davss0802@gmail.com>
2020-06-22 18:23:13 +0400

    Ball Launch and Rolling Sound

---
+++ Bowlmaster 3D Project/Assets/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+    public float laubchSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        Launch();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Launch()
+    {
+        rigidbody.velocity = new Vector3(0, 0, laubchSpeed);
+
+        audioSource.Play();
+    }
+}
