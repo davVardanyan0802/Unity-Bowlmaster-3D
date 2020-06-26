c4007681325adf800ae654ac8705e4feca17450e
david <davss0802@gmail.com>
2020-06-26 20:33:32 +0400

    Controll Arrows

--- Bowlmaster 3D Project/Assets/Scripts/Ball.cs
+++ Bowlmaster 3D Project/Assets/Scripts/Ball.cs
@@ -7,16 +7,18 @@ public class Ball : MonoBehaviour
     private Rigidbody rigidbody;
     private AudioSource audioSource;
     public Vector3 launchVelocity;
+    public bool inPlay = false;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         rigidbody.useGravity = false;
-       
+
     }
 
     public void Launch(Vector3 velocity)
     {
+        inPlay = true;
         rigidbody.useGravity = true;
         audioSource = GetComponent<AudioSource>();
         rigidbody.velocity = launchVelocity;
--- Bowlmaster 3D Project/Assets/Scripts/DragLaunch.cs
+++ Bowlmaster 3D Project/Assets/Scripts/DragLaunch.cs
@@ -6,15 +6,15 @@ using UnityEngine;
 public class DragLaunch : MonoBehaviour
 {
     private Ball ball;
-    private Vector3 dragStart,dragEnd;
-    private float startTime,endTime;
-   
+    private Vector3 dragStart, dragEnd;
+    private float startTime, endTime;
+
     void Start()
     {
         ball = GetComponent<Ball>();
     }
 
-    
+
     void Update()
     {
 
@@ -28,12 +28,21 @@ public class DragLaunch : MonoBehaviour
 
     public void DragEnd()
     {
-        dragEnd =  Input.mousePosition;
+        dragEnd = Input.mousePosition;
         startTime = Time.time;
         float dragDuration = endTime - startTime;
         float launchSpeedX = (dragEnd.x - dragStart.x) / dragDuration;
-        float launchSpeexZ = (dragEnd.y - dragStart.y) /dragDuration;
-        Vector3 launchVelocity = new Vector3(launchSpeedX,0,launchSpeexZ);
+        float launchSpeexZ = (dragEnd.y - dragStart.y) / dragDuration;
+        Vector3 launchVelocity = new Vector3(launchSpeedX, 0, launchSpeexZ);
         ball.Launch(launchVelocity);
+    }
+
+    public void MoveStart(float amount)
+    {
+        if (!ball.inPlay)
+        {
+            ball.transform.Translate(new Vector3(amount, 0, 0));
+        }
+
     }
 }
