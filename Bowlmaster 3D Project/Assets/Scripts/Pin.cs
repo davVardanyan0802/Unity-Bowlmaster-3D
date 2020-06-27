9969e1a1278a44a8eee3abe5331fc8a527c377cc
david <davss0802@gmail.com>
2020-06-27 16:05:46 +0400

    Displaying Pin Count

---
+++ Bowlmaster 3D Project/Assets/Scripts/Pin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float standingThreshold = 3f;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public bool IsStanding()
+    {
+        Vector3 rotationEuler = transform.rotation.eulerAngles;
+        float tiltInX = Mathf.Abs(rotationEuler.x);
+        float tiltInZ = Mathf.Abs(rotationEuler.z);
+
+        if (tiltInX < standingThreshold && tiltInZ < standingThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+}
---
+++ Bowlmaster 3D Project/Assets/Scripts/PinSetter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinSetter : MonoBehaviour
+{
+
+    public Text standingDisplay;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        standingDisplay.text = CountStanding().ToString();
+    }
+
+    int CountStanding()
+    {
+        int standing = 0;
+
+        foreach (Pin pin in GameObject.FindObjectsOfType<Pin>())
+        {
+            if (pin.IsStanding())
+            {
+                standing++;
+            }
+
+        }
+        return standing;
+    }
+}
